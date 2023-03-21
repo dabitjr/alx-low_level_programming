63b7cf5f4458dc72b7b50e5dde1698bdf83fc804
Victor Dabit <dabitjnr@gmail.com>
2023-03-20 17:35:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - Prints the size of various typesa based on 
+ * main - Prints the size of various typesa based on
  * the computer it is compiled and run on..
  * Return: Always 0.
  */
